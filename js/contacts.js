93158341f9325890f21115ec0a9b86b4e9750939
gaspard <gaspard@gmail.com>
2013-08-28 06:18:29 -0300

    contact: links from maps and pictures

--- js/contacts.js
+++ js/contacts.js
@@ -1,21 +1,32 @@
 var contacts ={}
 contacts.$obj = {};
+contacts.$mi = {};
+
 contacts.init = function(){
 	contacts.$obj = $("section.sec5");
+	contacts.$mi =  $(".maps .inner",contacts.$obj);
 
+	$("a",contacts.$mi).prop('target', '_blank').find("div").hover(
+		function(){ //hover
+			console.log(this)
+			$(this).stop().fadeTo('slow', 0.95)
+		},
+		function(){ //out
+			$(this).stop().fadeTo('slow', 1)
+		}
+	);
 
 	window.setTimeout(function(){
-		var $mi =  $(".maps .inner",contacts.$obj),
-			eh = $(".encart",contacts.$obj).height();
-		
+
+		var eh = $(".encart",contacts.$obj).height();
 		var mh = app.wh - eh;
-		
-		$(".maps .inner .m0").height(mh);
-		$(".maps .inner .q0").height(mh/2);
-		$(".maps .inner .q1").height(mh/2);
-		
-		var mih = $mi.height();
-		$mi.css({paddingTop:app.wh - mih})
+
+		$(".m0",contacts.$mi).height(mh);
+		$(".q0",contacts.$mi).height(mh/2);
+		$(".q1",contacts.$mi).height(mh/2);
+
+		var mih = contacts.$mi.height();
+		contacts.$mi.css({paddingTop:app.wh - mih})
 		$(".maps",contacts.$obj).height(app.wh).width(app.ww).show();
 
 	}.bind(this),1200);
