4243de3044824b4510beed5040cd868a07b73b39
gaspard <gaspard@gmail.com>
2013-09-13 03:13:12 -0300

    menu: activation

--- js/menu.js
+++ js/menu.js
@@ -16,7 +16,28 @@ var menu = {
 		$(".sec1").waypoint(function(direction){
 			if(direction == "down") menu.$navbar.stop().fadeIn()
 			else menu.$navbar.stop().fadeOut();
-		})
+		});
+		$(".sec2").waypoint(function(direction){
+			$('.sep',menu.$navbar).removeClass('active')
+			if(direction == "down") $($('.sep',menu.$navbar)[1]).addClass('active')
+			else $($('.sep',menu.$navbar)[0]).addClass('active')
+		});
+		$(".sec3").waypoint(function(direction){
+			$('.sep',menu.$navbar).removeClass('active')
+			if(direction == "down") $($('.sep',menu.$navbar)[2]).addClass('active')
+			else $($('.sep',menu.$navbar)[1]).addClass('active')
+		});
+		$(".sec4").waypoint(function(direction){
+			$('.sep',menu.$navbar).removeClass('active')
+			if(direction == "down") $($('.sep',menu.$navbar)[3]).addClass('active')
+			else $($('.sep',menu.$navbar)[2]).addClass('active')
+		});
+		$(".sec5").waypoint(function(direction){
+			$('.sep',menu.$navbar).removeClass('active')
+			if(direction == "down") $($('.sep',menu.$navbar)[4]).addClass('active')
+			else $($('.sep',menu.$navbar)[3]).addClass('active')
+		});
+
 	},
 	place : function(){
 		this.$brand = $(".brand",this.$navnar);
