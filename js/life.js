2f8da54d875887dabbb3553d2f898128196864e0
gaspard <gaspard@gmail.com>
2013-09-15 17:02:49 -0300

    life: linkify urls in tweets

--- js/life.js
+++ js/life.js
@@ -46,6 +46,12 @@ life.tweets = function(){
 			})
 		}
 
+		$(this).html(life.linkify($(this).text()))
 	});
 
+}
+
+life.linkify = function(text) {
+	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+	return text.replace(exp,"<a href=\"$1\" target=\"_blank\">$1</a>"); 
 }
