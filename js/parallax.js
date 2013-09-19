093469fa6320864b1a3b1df4390326acad1c0a71
gaspard <gaspard@gmail.com>
2013-09-19 12:51:28 -0300

    cleaning missing scroll stuffs

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,6 @@
 var parallax = {};
 parallax.objects = []
 parallax.coords = [];
-parallax.iScroll = null;
 parallax.init = function(){
 
 	// setting objects
@@ -43,33 +42,3 @@ parallax.update = function(s){
 		}
 	}
 }
-
-
-
-// window.requestAnimationFrame polyfill
-// shim layer with setTimeout fallback
-;(function() {
-	var lastTime = 0;
-	var vendors = ['webkit', 'moz', 'ms'];
-	for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-		window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-		window.cancelAnimationFrame =
-			window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-	}
-
-	if (!window.requestAnimationFrame)
-		window.requestAnimationFrame = function(callback, element) {
-		var currTime = new Date().getTime();
-		var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-		var id = window.setTimeout(
-			function() { callback(currTime + timeToCall); },
-			timeToCall);
-		lastTime = currTime + timeToCall;
-		return id;
-	};
-
-	if (!window.cancelAnimationFrame)
-	window.cancelAnimationFrame = function(id) {
-		clearTimeout(id);
-	};
-}());
