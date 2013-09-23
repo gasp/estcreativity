f31cbce56c93618eeeda1a5144365ceed92982fa
gaspard <gaspard@gmail.com>
2013-09-23 08:51:01 -0300

    emails: including the js dependancy

---
+++ js/emails.js
@@ -0,0 +1,15 @@
+var emails = {};
+emails.init = function(){
+	var that = this,
+		$contactmail = $('section.sec5 .encart .email');
+	$('.reach a.email').each(function(){
+		that.set(this)
+	});
+	$contactmail.html(that.set($contactmail));
+};
+emails.set = function(o){
+	var e = $(o).data('email').toString() + '@estcreativity.com';
+	var a = 'mailto:' +  e;
+	$(o).attr({'href': a});
+	return e;
+}
